6c4a513a621edce98722b8b7ed35bfac4b42ea4c
khansa-qonita <khansaqonita420@gmail.com>
2026-05-12 18:01:18 +0700

    menambahkan member untuk class ibu

--- ibu.h
+++ ibu.h
@@ -1,10 +1,22 @@
 #ifndef IBU_H
 #define IBU_H
 #include <vector>
+#include <iostream>
+using namespace std;
+
 
 class ibu{
 public:
     string nama;
     vector<anak*> daftar_anak;
-    
-}
+
+    ibu(string pNama) :nama(pNama) {
+        cout <<"Ibu \" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+
